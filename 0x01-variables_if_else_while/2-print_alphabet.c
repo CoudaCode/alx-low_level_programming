1828dc8f4ed8b6f2eec4fbd3036d2c9ae55eed49
Diarassouba Mamadou <couda.dm@gmail.com>
2023-07-14 19:01:48 -0700

    demo

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -Enter point
+ * Description : "Use putchar()"
+ * Return : always 0.
+ */
+
+int main(void)
+{
+	char* n; 
+	n = "abcdefghijklmnopqrstuvwxyz";
+	
+	putchar(*n);
+	return (0);
+}
