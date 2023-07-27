e233813e86f45e5229ddbb7ee8bcd4374e937b76
Diarassouba Mamadou <couda.dm@gmail.com>
2023-07-26 18:31:19 -0700

    dmeo

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - enter point
+ * Description print 0123456789
+ * Return: Always 0
+ */
+int main(void)
+{
+	int number = 0;
+
+	for( number = 0; number < 10; number++)
+	{
+		printf("%d",number);
+	}
+
+	return (0);
+}
