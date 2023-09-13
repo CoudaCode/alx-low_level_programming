875981c16735626d93fc5e561b842b0e465e9acd
CoudaCode <couda.dm@gmail.com>
2023-09-13 02:55:00 -0700

    ok

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * _isalpha - check is params is lowercase and uppercase
+ * @c : the charactere to check
+ * Return: 1 if charac is lowerse else 0
+ */
+int _isalpha(int c){
+	return ((c > 'a' || c < 'z') || (c > 'A' || c < 'Z'));
+}
