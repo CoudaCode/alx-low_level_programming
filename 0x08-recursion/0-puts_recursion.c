8ff128c30054bdfd358f99349861772950b6e751
Diarassouba Mamadou <couda.dm@gmail.com>
2023-10-25 02:51:15 -0700

    modif

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - Prints a string followed by a new line
+ * @s: string
+ *
+ * Return: On success 1.
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+	_putchar('\n');
+}
