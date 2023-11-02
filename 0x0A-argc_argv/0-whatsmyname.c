ef9e8a70dab60d14834e33a3044a44f8405e46f6
Diarassouba Mamadou <couda.dm@gmail.com>
2023-11-02 10:41:07 -0700

    demo

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * main - function
+ * @argc: offset count
+ * @argv: offset value
+ * Return: 0
+ */
 int main(int argc, char *argv[])
 {
 	if (argc > 0)
