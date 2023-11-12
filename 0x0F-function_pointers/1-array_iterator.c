2242aa7f21b70b826ad1f4b847c5a96abd171d74
Diarassouba Mamadou <couda.dm@gmail.com>
2023-11-11 23:36:39 -0800

    ok

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - an array iterator
+ * @array: a pointer to an array
+ * @size: an array size
+ * @action: an action to do
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	if (array == NULL || action == NULL)
+		return;
+	while (i < size)
+	{
+		action(array[i]);
+		i++;
+	}
+}
